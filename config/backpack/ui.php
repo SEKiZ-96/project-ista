978d41218f90231be5c9d906cc6c0ab87d00593a
SEKiZ-96 <Anassadil117@gmail.com>
2024-04-22 11:20:30 +0100

    test

--- config/backpack/ui.php
+++ config/backpack/ui.php
@@ -46,7 +46,7 @@ return [
     // ----
 
     // Project name - shown in the window title
-    'project_name' => 'Backpack Admin Panel',
+    'project_name' => 'Schedule Maker',
 
     // Content of the HTML meta robots tag to prevent indexing and link following
     'meta_robots_content' => 'noindex, nofollow',
@@ -60,10 +60,10 @@ return [
     // The string below will be passed through the url() helper.
     // - default: '' (project root)
     // - alternative: 'admin' (the admin's dashboard)
-    'home_link' => '',
+    // 'home_link' => '',
 
     // Menu logo. You can replace this with an <img> tag if you have a logo.
-    'project_logo' => '<b>SEK</b>IZ',
+    'project_logo' => '<b class="text-success">SCHEDULE</b> <b class="text-warning" >MAKER</b>',
 
     // Show / hide breadcrumbs on admin panel pages.
     'breadcrumbs' => true,
